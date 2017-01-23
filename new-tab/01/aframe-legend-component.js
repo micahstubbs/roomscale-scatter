9d402da0b47448437cc18f64c9ff5763a8a0686e
Micah Stubbs <micah.stubbs@gmail.com>
2017-01-23 14:03:08 -0800

    add new tab iteration. use js to set config for scatter-component

---
+++ new-tab/01/aframe-legend-component.js
@@ -0,0 +1,86 @@
+AFRAME.registerComponent('legend', {
+  schema: {
+    csv: {
+      type: 'string'
+    },
+    colorVariable: {
+      type: 'string'
+    },
+    colorVariableDomain: {
+      type: 'array'
+    },
+    colors: {
+      type: 'array'
+    },
+    legendItemYOffset: {
+      type: 'number',
+      default: 0.5
+    }     
+  },
+  /**
+   * Called once when component is attached. Generally for initial setup.
+   */
+  update() {
+    // entity attributes
+    const el = this.el;
+    const options = this.data;
+    const colorVariable = options.colorVariable;
+    const legendItemYOffset = options.legendItemYOffset;
+
+    let colors;
+    if (
+      typeof options.colors !== 'undefined' &&
+      options.colors.length > 0
+      ) {
+      colors = options.colors;
+    } else {
+      colors = d3.schemeCategory10;
+    }
+    console.log('colors', colors);
+
+    const colorScale = d3.scaleOrdinal()
+      .range(colors);
+
+    const legendEntity = d3.select(el)
+      .append('a-entity')
+      .attr('class', 'legend')
+      .attr('position', '1.4 0.5 -1.5')
+      .attr('rotation', '0 -45 0')
+      .attr('scale', '0.8 0.8 0.8');
+
+    d3.csv(options.csv, data => {
+      // allow user to specify colorVariableDomain
+      // to control sort order of legend items
+      let colorVariableDomain;
+      if (
+        typeof options.colorVariableDomain !== 'undefined' &&
+        options.colorVariableDomain.length > 0
+      ) {
+        colorVariableDomain = options.colorVariableDomain;
+      } else {
+        colorVariableDomain = data.map(d => d[colorVariable]).filter(onlyUnique);
+      } 
+      colorScale.domain(colorVariableDomain);
+
+      // draw legend text and a legend icon
+      // for all colorVariableDomain values
+      legendEntity
+        .selectAll('.legendItem')
+        .data(colorVariableDomain)
+        .enter().append('a-entity')
+          .attr('position', (d, i) => `0 ${(colorVariableDomain.length - i - 1) * legendItemYOffset} 0`)
+          .attr('bmfont-text', d => `text: ${d}; color: ${colorScale(d)}`)
+          .append('a-sphere')
+            .attr('radius', '0.03')
+            .attr('position', '-0.1 0.05 0')
+            .attr('color', d => colorScale(d));
+    })
+  }
+});
+
+/**
+ * onlyUnique() tests if values in an array are unique
+ */
+function onlyUnique(value, index, self) { 
+  return self.indexOf(value) === index;
+}
